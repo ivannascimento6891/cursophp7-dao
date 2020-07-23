5223bf4f91e9611c5d0783832f7148eb3432a612
Ivan Rodrigues <ivanrodrigues.info@gmail.com>
2020-07-23 15:01:15 -0300

    Foi criado a classu usuarios

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 
 require_once("config.php");
 
-$sql = new sql();
+$root = new usuarios();
+
+$root->loadbyId(2);
+
+echo $root;
+/*$sql = new sql();
 
 $sql->query("");
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
-
+*/
  ?>
