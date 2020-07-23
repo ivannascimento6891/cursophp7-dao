b76d622bb1eb0eb2c78fb8696b2b681a09e1c7cf
Ivan Rodrigues <ivanrodrigues.info@gmail.com>
2020-07-23 15:42:49 -0300

    Métodos de listar

--- index.php
+++ index.php
@@ -2,17 +2,28 @@
 
 require_once("config.php");
 
-$root = new usuarios();
 
-$root->loadbyId(2);
+//Carrega um usuario
+//$root = new usuarios();
+//$root->loadbyId(2);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuarios
+//$lista = usuarios::getList();
+///echo json_encode($lista);
+
+//Carrega uma lista de usarios buscando pelo login
+//$search = usuarios::search("Pe");
+//echo json_encode($search);
+
+//Carrega usuario usando login e senha
+$usuarios = new usuarios();
+$usuarios->login("Petra","12345");
+echo $usuarios;
+
 /*$sql = new sql();
-
 $sql->query("");
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 */
  ?>
